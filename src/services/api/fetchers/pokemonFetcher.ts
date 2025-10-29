1e687b44b2bc601d29af05e5d4c379c7fc275db1
bradcarthew <bradley.carthew@theodo.com>
2025-10-29 17:54:19 +0200

    chore: add dotenv variable for pokeapi url

--- src/services/api/fetchers/pokemonFetcher.ts
+++ src/services/api/fetchers/pokemonFetcher.ts
@@ -10,12 +10,14 @@ export interface PokemonApiResponse {
   }[];
 }
 
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
 export const fetchPokemonPage = async (
   context: QueryFunctionContext
 ): Promise<PokemonApiResponse> => {
   const { pageParam = 0 } = context;
   const limit = 20;
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${pageParam}`);
+  const res = await fetch(`${API_URL}/pokemon?limit=${limit}&offset=${pageParam}`);
 
   if (!res.ok) {
     throw new Error('Failed to fetch Pokémon');

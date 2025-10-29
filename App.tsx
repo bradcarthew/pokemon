de83b7e2d1c33f04f89cf82e9c594c65e73ce1a1
bradcarthew <bradley.carthew@theodo.com>
2025-10-29 11:07:16 +0200

    refactor: move code into new Pokedex component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Pokedex from './src/components/Pokedex';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is a Pokemon app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Pokedex />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

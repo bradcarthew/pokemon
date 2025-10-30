a0467df67637816e5f27f71d5e5e323e0c724d52
bradcarthew <bradley.carthew@theodo.com>
2025-10-30 08:05:01 +0200

    chore: remove unused style from PokemonDetails

--- src/screens/PokemonDetails/PokemonDetails.styles.ts
+++ src/screens/PokemonDetails/PokemonDetails.styles.ts
@@ -4,13 +4,5 @@ export const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 24,
-  },
-  text: {
-    fontSize: 32,
-    fontWeight: "bold",
-    textTransform: "capitalize",
-    marginBottom: 20,
-    color: "#333",
-    transform: 'capitalize'
-  },
+  }
 });
